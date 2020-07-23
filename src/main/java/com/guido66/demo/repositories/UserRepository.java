ecbe61ada973ee9b5787bcc3312e55a1b4c30e32
Trollfast <guilherme.fast2503@gmail.com>
2020-07-23 14:29:53 -0300

    Service Layer, component registration

--- src/main/java/com/guido66/demo/repositories/UserRepository.java
+++ src/main/java/com/guido66/demo/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package com.guido66.demo.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.guido66.demo.entities.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
