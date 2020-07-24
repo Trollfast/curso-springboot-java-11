6f188cc422d54553df840f8804e41131c33602c5
Trollfast <guilherme.fast2503@gmail.com>
2020-07-24 13:34:20 -0300

    Category

---
+++ src/main/java/com/guido66/demo/repositories/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.guido66.demo.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.guido66.demo.entities.Category;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
